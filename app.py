29dbd0c9dcc898bebf5b291da804591f31221a91
andreaxricci <62494809+andreaxricci@users.noreply.github.com>
2023-06-29 15:06:28 +0200

    minimal functioning product

--- app.py
+++ app.py
@@ -1,40 +1,44 @@
 import base64
 
-
 import streamlit as st
+from openai.error import OpenAIError
 
-from utils import text_split, parse_pdf
+from utils import text_split, parse_pdf, get_embeddings, get_sources, get_answer
 
 
 st.set_page_config(page_title="pdf-GPT", page_icon="📖", layout="wide")
 st.header("pdf-GPT")
 
+
 def clear_submit():
     st.session_state["submit"] = False
 
 
-def displayPDF(uploaded_file):
-    
+def displayPDF(upl_file):
     # Read file as bytes:
-    bytes_data = uploaded_file.getvalue()
+    bytes_data = upl_file.getvalue()
 
     # Convert to utf-8
-    base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
+    base64_pdf = base64.b64encode(bytes_data).decode("utf-8")
 
     # Embed PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
     # Display file
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="file_qa_api_key", type="password")
-    
+    openai_api_key = st.text_input(
+        "OpenAI API Key", key="file_qa_api_key", type="password"
+    )
+
     uploaded_file = st.file_uploader(
-        "Upload file", type=["pdf"], 
-        help="Only PDF files are supported", 
-        on_change=clear_submit)
+        "Upload file",
+        type=["pdf"],
+        help="Only PDF files are supported",
+        on_change=clear_submit,
+    )
 
 
 col1, col2 = st.columns(spec=[2, 1], gap="small")
@@ -43,11 +47,19 @@ col1, col2 = st.columns(spec=[2, 1], gap="small")
 if uploaded_file:
     with col1:
         displayPDF(uploaded_file)
-    
+
     with col2:
         pdf_document = parse_pdf(uploaded_file)
         text = text_split(pdf_document)
+        try:
+            with st.spinner("Indexing document... This may take a while⏳"):
+                vs = get_embeddings(text,openai_api_key)
+            st.session_state["api_key_configured"] = True
+        except OpenAIError as e:
+            st.error(e._message)
         
+        
+
         question = st.text_input(
             "Ask something about the file",
             placeholder="Can you give me a short summary?",
@@ -59,4 +71,19 @@ if uploaded_file:
             st.info("Please add your OpenAI API key to continue.")
 
         if uploaded_file and question and openai_api_key:
-            st.info("Answer")
+
+            try:
+                sources = get_sources(vs, question)
+                answer = get_answer(sources, question, openai_api_key)
+                #sources = get_sources(answer, sources)
+                st.markdown("#### Answer")
+                st.markdown(answer["output_text"].split("SOURCES: ")[0])
+
+                #st.markdown("#### Sources")
+                #for source in sources:
+                #    st.markdown(source.page_content)
+                #    st.markdown(source.metadata["source"])
+                #    st.markdown("---")
+
+            except OpenAIError as e:
+                st.error(e._message)
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, Dict, List
 
 from io import BytesIO
 import re
@@ -7,9 +7,22 @@ from pypdf import PdfReader
 
 from langchain.docstore.document import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import ChatVectorDBChain, LLMChain
+from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import VectorStore
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
 
+NUM_CHUNKS = 5
+model_name = 'gpt-3.5-turbo'
 
 def parse_pdf(file: BytesIO) -> List[str]:
+    ''' PDF parser '''
     pdf = PdfReader(file)
     output = []
     for page in pdf.pages:
@@ -25,9 +38,14 @@ def parse_pdf(file: BytesIO) -> List[str]:
 
     return output
 
-def text_split(text: str ) -> List[Document]:
-    """Converts a string or list of strings to a list of Documents (chunks of fixed size)
-    with metadata."""
+
+def text_split(text: str) -> List[Document]:
+    """Converts a string to a list of Documents (chunks of fixed size)
+    and returns this along with the following metadata:
+        - page number
+        - chunk number
+        - source (concatenation of page & chunk number)
+    """
 
     if isinstance(text, str):
         text = [text]
@@ -39,10 +57,10 @@ def text_split(text: str ) -> List[Document]:
 
     doc_chunks = []
     text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 2048,
-    chunk_overlap  = 20,
-    #separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
-    length_function = len,
+        chunk_size=2048,
+        chunk_overlap=20,
+        separators=["\n\n", "\n", ".", "!", "?", ",", " ", ""],
+        length_function=len,
     )
 
     for doc in page_docs:
@@ -54,5 +72,72 @@ def text_split(text: str ) -> List[Document]:
             # Add sources a metadata
             doc.metadata["source"] = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
             doc_chunks.append(doc)
-    print(doc_chunks)
+
     return doc_chunks
+
+
+def get_embeddings(docs: List[Document], openai_api_key: str) -> VectorStore:
+    vectorstore = Chroma.from_documents(docs, OpenAIEmbeddings(openai_api_key=openai_api_key), ids=[doc.metadata["source"] for doc in docs])
+
+    return vectorstore
+
+
+def get_sources(vectorstore: VectorStore, query: str) -> List[Document]:
+    # Search for similar chunks
+    docs = vectorstore.similarity_search(query, k=5)
+
+    return docs
+
+def get_answer(docs: List[Document], query: str, openai_api_key: str) -> Dict[str, Any]:
+    """Gets an answer to a question from a list of Documents."""
+
+    # Get the answer
+
+    chain = load_qa_with_sources_chain(
+        OpenAI(
+            temperature=0, openai_api_key=openai_api_key
+        ),  # type: ignore
+        chain_type="stuff",
+        prompt=STUFF_PROMPT,
+    )
+
+    # Cohere doesn't work very well as of now.
+    # chain = load_qa_with_sources_chain(
+    #     Cohere(temperature=0), chain_type="stuff", prompt=STUFF_PROMPT  # type: ignore
+    # )
+    answer = chain(
+        {"input_documents": docs, "question": query}, return_only_outputs=True
+    )
+    return answer
+
+
+
+
+## Use a shorter template to reduce the number of tokens in the prompt
+template = """Create a final answer to the given questions using the provided document excerpts(in no particular order) as references. ALWAYS include a "SOURCES" section in your answer including only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not know. Do not attempt to fabricate an answer and leave the SOURCES section empty.
+
+---------
+
+QUESTION: What  is the purpose of ARPA-H?
+=========
+Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
+Source: 1-32
+Content: While we’re at it, let’s make sure every American can get the health care they need. \n\nWe’ve already made historic investments in health care. \n\nWe’ve made it easier for Americans to get the care they need, when they need it. \n\nWe’ve made it easier for Americans to get the treatments they need, when they need them. \n\nWe’ve made it easier for Americans to get the medications they need, when they need them.
+Source: 1-33
+Content: The V.A. is pioneering new ways of linking toxic exposures to disease, already helping  veterans get the care they deserve. \n\nWe need to extend that same care to all Americans. \n\nThat’s why I’m calling on Congress to pass legislation that would establish a national registry of toxic exposures, and provide health care and financial assistance to those affected.
+Source: 1-30
+=========
+FINAL ANSWER: The purpose of ARPA-H is to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
+SOURCES: 1-32
+
+---------
+
+QUESTION: {question}
+=========
+{summaries}
+=========
+FINAL ANSWER:"""
+
+STUFF_PROMPT = PromptTemplate(
+    template=template, input_variables=["summaries", "question"]
+)
