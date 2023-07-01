8721f4a1514782f0d4aa19ac99bbc19922523817
andreaxricci <62494809+andreaxricci@users.noreply.github.com>
2023-07-01 17:29:36 +0200

    updated template to better handle sources

--- prompts.py
+++ prompts.py
@@ -1,29 +1,36 @@
 from langchain.prompts import PromptTemplate
 
 ## Use a shorter template to reduce the number of tokens in the prompt
-template = """Create a final answer to the given questions using the provided document excerpts(in no particular order) as references. ALWAYS include a "SOURCES" section in your answer including only the minimal set of sources needed to answer the question. If you are unable to answer the question, simply state that you do not know. Do not attempt to fabricate an answer and leave the SOURCES section empty.
+template = """
+"You are an intelligent assistant helping users with questions about the document. " + \
+"Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
+"Answer the following question using only the data provided in the sources below. " + \
+"For tabular information return it as an html table. Do not return markdown format. "  + \
+"Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. " + \
+"If you cannot answer using the sources below, say you don't know. " + \
 
----------
 
-QUESTION: What  is the purpose of ARPA-H?
-=========
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
-Source: 1-32
-Content: While we’re at it, let’s make sure every American can get the health care they need. \n\nWe’ve already made historic investments in health care. \n\nWe’ve made it easier for Americans to get the care they need, when they need it. \n\nWe’ve made it easier for Americans to get the treatments they need, when they need them. \n\nWe’ve made it easier for Americans to get the medications they need, when they need them.
-Source: 1-33
-Content: The V.A. is pioneering new ways of linking toxic exposures to disease, already helping  veterans get the care they deserve. \n\nWe need to extend that same care to all Americans. \n\nThat’s why I’m calling on Congress to pass legislation that would establish a national registry of toxic exposures, and provide health care and financial assistance to those affected.
-Source: 1-30
-=========
-FINAL ANSWER: The purpose of ARPA-H is to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
-SOURCES: 1-32
+###
+Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
 
----------
+Sources:
+1-32: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
+1-31: Overlake is in-network for the employee plan.
+1-30: Overlake is the name of the area that includes a park and ride near Bellevue.
+1-29: In-network institutions include Overlake, Swedish and others in the region
 
-QUESTION: {question}
-=========
+Answer:
+In-network deductibles are $500 for employee and $1000 for family [1-32] and Overlake is in-network for the employee plan [1-31][1-29].
+
+###
+Question: '{question}'?
+
+Sources:
 {summaries}
-=========
-FINAL ANSWER:"""
+
+Answer:
+"""
+
 
 STUFF_PROMPT = PromptTemplate(
     template=template, input_variables=["summaries", "question"]
